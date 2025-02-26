af9761245799592ff231280050e609484159490c
zhdanova.marina.de@gmail.com <zhdanova.marina.de@gmail.com>
2025-02-26 16:07:09 +0000

    percents

--- 04-analytics-engineering/taxi_rides_ny/models/core/fct_taxi_trips_monthly_fare_p95.sql
+++ 04-analytics-engineering/taxi_rides_ny/models/core/fct_taxi_trips_monthly_fare_p95.sql
@@ -13,15 +13,32 @@ with fact_trips_data as (
   from {{ ref('fact_trips') }}
   where fare_amount > 0 
     and trip_distance > 0 
-    and payment_type_description in ('Cash', 'Credit Card')
-)
+    and payment_type_description in ('Cash', 'Credit card')
+),
 
+fact_trips_data_percent AS (
 SELECT 
     service_type,
     year,
     month,
     PERCENTILE_CONT(fare_amount, 0.97) OVER (
         PARTITION BY service_type, year, month
-    ) AS p97
-FROM fact_trips_data;
+    ) AS p97,
+    PERCENTILE_CONT(fare_amount, 0.95) OVER (
+        PARTITION BY service_type, year, month
+    ) AS p95,
+    PERCENTILE_CONT(fare_amount, 0.90) OVER (
+        PARTITION BY service_type, year, month
+    ) AS p90
+FROM fact_trips_data
+)
 
+SELECT 
+    service_type,
+    year,
+    month,
+    ANY_VALUE(p97) AS p97,
+    ANY_VALUE(p95) AS p95,
+    ANY_VALUE(p90) AS p90
+FROM fact_trips_data_percent
+group by service_type, year, month
